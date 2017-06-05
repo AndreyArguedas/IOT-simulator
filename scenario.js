3c5dc8863c6391f74a095fe3d511b5bd8c41c58b
Andrey Arguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-06-04 21:48:43 -0600

    Entregado

--- scenario.js
+++ scenario.js
@@ -4,57 +4,57 @@ function Scenario(){
 		if(this.level === 1){
 			image(city1, -x * 2 * 2,0,4267,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia1.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia1.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 2){
 			image(city2,-x * 2 * 2,0,2580,650); //Se dibuja el fondo //2080
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia2.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia2.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 3){
 			image(city3,-x * 2 * 2,0,2778,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia3.png'></img><br><img src='topologias/topologia16.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia3.png'></img><br><img src='topologias/topologia15.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 4){
 			image(city5,-x * 2 * 2,0,2880,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia4.png'></img><br><br><br><img src='topologias/topologia15.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia4.png'></img><br><br><br><img src='topologias/topologia14.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 5){
 			image(city6,-x * 2 * 2,0,4268,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia6.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia6.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 6){
 			image(city7,-x * 2 * 2,0,4222,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia8.png'></img><br><br><br><img src='topologias/topologia7.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia6.png'></img><br><br><br><img src='topologias/topologia7.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 7){
 			image(city8,-x * 2 * 2,0,2880,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topoogia9.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia8.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 8){
 			image(city9,-x * 2 * 2,0,3072,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia10.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia9.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 9){
 			image(city10,-x * 2 * 2,0,2580,650); //Se dibuja el fondo
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia13.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia12.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 10){
 			image(cityRestric,-x * 2 * 2,0,2214,650); //CIUDAD CON PARTE
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia13.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia12.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		else if(this.level === 11){
 			image(city11,-x * 2 * 2,0,3800,650); //CIUDAD DONDE ESTA EL POLICIA
 			$("#myModal5Message").html("");
-			$("#myModal5Message").html("<img src='topologias/topologia12.png'></img>");
+			$("#myModal5Message").html("<img src='topologias/topologia11.png'></img><br><br><br><img src='topologias/topologia13.png'></img>");
 		}
 		
 	}
